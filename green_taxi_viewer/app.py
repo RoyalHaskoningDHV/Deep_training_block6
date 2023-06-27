1d978fcec5aeb7b040092bf87c9fd31fa8b1c391
Steffen Buergers <steffen.burgers@rhdhv.com>
2023-06-27 19:02:35 +0200

    simple flask app with docker

---
+++ green_taxi_viewer/app.py
@@ -0,0 +1,72 @@
+import io
+import matplotlib.pyplot as plt
+import datetime as dt
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+def predict_trip_costs(h):
+    """This is a dummy prediction function, which always predicts
+    the average of the next 24 hours copied by hand from synapse.
+    In reality, you could have a machine learning model file as part of
+    the repository and use that to either train or make predictions.
+
+    Parameters
+    ----------
+    h : int
+        Hour of the day
+
+    Returns
+    -------
+    list
+        Expected normalized trip cost for the coming 24 hours.
+    """
+    pred = [
+        0.0208365, 0.02058832, 0.01781128, 0.01946697, 0.01930636,
+        0.01736085, 0.01729656, 0.01700394, 0.01756695, 0.01713748,
+        0.01806401, 0.01859311, 0.02019171, 0.01891349, 0.02013918,
+        0.02105339, 0.02070073, 0.02223452, 0.02666162, 0.0240056,
+        0.02165742, 0.01765045, 0.01760978, 0.01764494,
+    ] * 2
+
+    return pred[h:h + 24]
+
+
+def create_figure(y):
+    """Make a lineplot of y and save it as static figure
+
+    Parameters
+    ----------
+    y : list
+    """
+    x = range(0, len(y))
+    fig = plt.figure()
+    plt.plot(x, y)
+    plt.xlabel("Hours from now")
+    plt.ylabel("Expected costs")
+    plt.title("Expected price for the coming 24 hours")
+    fig.savefig('./static/images/price_plot.png')
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
+def main():
+
+    if request.method == 'POST':
+
+        hour = request.form.get('hour')
+        if hour == "now":
+            hour = dt.datetime.now().hour
+
+        create_figure(predict_trip_costs(hour))
+        return render_template('home.html', url='/static/images/price_plot.png')
+
+    else:
+        return render_template('home.html', url=None)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
